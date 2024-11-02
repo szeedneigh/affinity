e73207d53f6cd0e010e45566ffb59401f4834502
szeedneigh <sidneyjohn1704@gmail.com>
2024-11-02 09:56:35 +0800

    Added and modified shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,19 @@ const App = () => {
   return (
     <main className='flex h-screen'>
       <Routes>
+        
         {/* public routes */}
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-un" element={<SignupForm />} />
         </Route>
         
-
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />}/>
         </Route>
       </Routes>
+
     </main>
   )
 }
