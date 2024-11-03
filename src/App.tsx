db0cafae625ebda7b87671e7d437b56b372169ce
szeedneigh <sidneyjohn1704@gmail.com>
2024-11-03 23:26:54 +0800

    Modified the file layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import './globals.css';
-import SignInForm from './_auth/forms/SignInForm';
-import SignUpForm from './_auth/forms/SignUpForm';
-import { Home } from './_root/pages';
-import AuthLayout from './_auth/AuthLayout';
-import RootLayout from './_root/RootLayout';
+import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <main className='flex h-screen'>
+    <main className="flex h-screen">
       <Routes>
-
         {/* public routes */}
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SignInForm />} />
           <Route path="/sign-up" element={<SignUpForm />} />
         </Route>
-        
+
         {/* private routes */}
         <Route element={<RootLayout />}>
-          <Route index element={<Home />}/>
+          <Route index element={<Home />} />
         </Route>
       </Routes>
-
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -4,17 +4,21 @@ const AuthLayout = () => {
   const isAuthenticated = false;
   return (
     <>
-    {isAuthenticated ? (
-      <Navigate to="/" />
-    ) : (
-      <>
-        <section>
-          <Outlet />
-        </section>
-      </>
-    )}
-  </>
-  )
-}
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
 
-export default AuthLayout
+          <img
+          src=""
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
