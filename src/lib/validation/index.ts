fd73f2d7fe7e0632933cc9bdd5476cc2a7833655
szeedneigh <sidneyjohn1704@gmail.com>
2024-11-06 23:38:53 +0800

    Added index validation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,8 +1,8 @@
 import { z } from "zod"
 
 export const SignupValidation = z.object({
-  name: z.string().min(2, { message: 'Too short' }),
-  username: z.string().min(2, { message: 'Too short'}),
+  name: z.string().min(2, { message: 'Name should only contain letters and spaces.' }),
+  username: z.string().min(2, { message: 'Username can contain letters, numbers, and underscores only.'}),
   email: z.string().email(),
-  password: z.string().min(8, { message: 'Password must be atleast 8 characters' }),
+  password: z.string().min(8, { message: 'Password must be atleast 8 characters long' }),
 })
