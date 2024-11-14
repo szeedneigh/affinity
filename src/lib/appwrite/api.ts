4b2ba3a8396c0bac2d81ad29c9345780743f724f
szeedneigh <sidneyjohn1704@gmail.com>
2024-11-14 22:51:59 +0800

    Modified appwrite

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,7 @@
-import { ID } from 'appwrite';
+import { ID } from "appwrite";
 
 import { INewUser } from "@/types";
-import { account } from './config';
+import { account, appwriteConfig, databases } from "./config";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -10,12 +10,75 @@ export async function createUserAccount(user: INewUser) {
       user.email,
       user.password,
       user.name
-    )
+    );
 
-    return newAccount;
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      userName: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
-    
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function signInAccount(user: { email: string; password: string }) {
+  try {
+    const session = await account.createEmailSession(user.email, user.password);
+
+    return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get();
+
+    if (!currentAccount) throw Error;
+
+    const currentAccount = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    )
+
+    if(!currentUser) throw Error;
+
+    return currentUser.documents[0];
+
+  } catch (error) {
+    console.log(error);
   }
 }
